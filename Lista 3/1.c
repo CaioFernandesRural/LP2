33640522849426c5ab328ad2f48f2ab540bd4033
Caio <caioorebeo@gmail.com>
2022-02-21 20:20:41 -0300

    1.2 completa

--- Lista 3/1.c
+++ Lista 3/1.c
@@ -2,21 +2,38 @@
 
 int main(){
 
-    int n,es,nfat;
-    long int fat(int f);
-    int pot(int x,int y);
+    int n,es,nfat,x,y,xpoty,xmulty;
+    long int fat(int n);
+    int pot(int x, int y);
+    int mult(int x, int y);
 
     while(1){
-        printf("\nDigite N: ");
-        scanf("%d",&n);
+
         printf(
         "\n1) Fatorial de N \n2) X^Y \n3) X*Y \n4) X mod Y \n5) Sequencia de N termos de Fibonacci \n6) Fim \nEscolha: "
         );
         scanf("%d",&es);
 
         if (es==1){
+            printf("\nDigite N: ");
+            scanf("%d",&n);
+
             nfat=fat(n);
             printf("\n%d! = %ld\n",n,fat(n));
+        }
+
+        else if (es==2){
+            printf("\nDigite X: ");
+            scanf("%d",&x);
+            printf("Digite Y: ");
+            scanf("%d",&y);
+
+            xpoty=pot(x,y);
+            printf("\n%d ^ %d = %d\n",x,y,xpoty);
+        }
+
+        else if (es==3){
+
         }
     }
 
@@ -34,3 +51,13 @@ long int fat(int n){
     }
 
 }
+int pot(int x,int y){
+
+    if (y==0){
+        return 1;
+    }
+    else{
+        return(x*pot(x,y-1));
+    }
+}
+int mult(int x, int y);
