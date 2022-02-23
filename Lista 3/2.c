1e98baedd6dd9733f0f4a532d57a8d16272c3872
Caio <caioorebeo@gmail.com>
2022-02-22 22:56:49 -0300

    porra

--- Lista 3/2.c
+++ Lista 3/2.c
@@ -2,40 +2,28 @@
 
 int main(){
 
-    float x;
-    float maio(float x);
-    float men(float x);
-    
+    float x,res,sub,mul;
+    res=0;
+    mul=1;
+
     printf("Digite o valor de x: ");
     scanf("%f",&x);
 
     if (x>0){
 
-        printf("Y = %.2f",maio(x));
-    }
-    else {
-        
-        printf("Y = %.2f",men(x));
-    }
-
-}
-float maio(float x){
-    
-    float res=0;
-    
-    for(int som=1;som<=100;som++){
+        for(int som=1;som<=100;som++){
         
         res+=((x+som)/som);
+        }
     }
-    return res;
-}
-float men(float x){
-    
-    float sub=100,mul=1,res=0;
-
+    else {
+        sub=100;
         for(int som=1;som<=100;som++){
             res+=(mul*(((x*x)+som)/sub));
             sub--;
             mul*=-1;
         }
+    }
+    printf("Y = %.2f",res);
+
 }
