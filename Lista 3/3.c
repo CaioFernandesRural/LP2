f95518318972662ed406a4a0f3f6e55b92304ce9
Caio <caioorebeo@gmail.com>
2022-02-22 23:27:01 -0300

    parte da 3

--- Lista 3/3.c
+++ Lista 3/3.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+int div(int n)
+{
+    int tot = 0;
+    printf("Divisores de %d: ",n);
+    for (int c=1;c<=n;c++)
+    {
+        if (n%c==0)
+        {
+            printf(" %d ", c);
+            tot++;
+        }
+    }
+    return tot;
+}
+
+int main(){
+
+    int n;
+
+    printf("Número(negativo para): ");
+    scanf("%d",&n);
+
+    if (n%2==0){
+        printf("= %d divisores\n",div(n));
+    }
+
+}
