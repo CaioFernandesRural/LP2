177d3844029604b43f9e5ade6a1158dcbaf35074
Caio <caioorebeo@gmail.com>
2022-02-22 23:11:20 -0300

    2 salva

--- Lista 3/2.c
+++ Lista 3/2.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+float mai(float x)
+{
+    float total=0;
+    for (int i=0; i<=100;i++)
+    {
+        total+=(x+i)/i;
+    }
+    return total;
+}
+float men(float x)
+{
+    int sub=100;
+    float total=0;
+    int mul=1;
+    for (int i=1;i<=100;i++)
+    {
+        total+=mul*((x*x)+i)/sub;
+        sub--;
+        mul*=-1;
+    }
+    return total;
+}
+
 int main(){
 
     float x,res,sub,mul;
@@ -9,20 +32,11 @@ int main(){
     printf("Digite o valor de x: ");
     scanf("%f",&x);
 
-    if (x>0){
-
-        for(int som=1;som<=100;som++){
-        
-        res+=((x+som)/som);
-        }
+    if (x>0){ 
+        res=mai(x);
     }
-    else {
-        sub=100;
-        for(int som=1;som<=100;som++){
-            res+=(mul*(((x*x)+som)/sub));
-            sub--;
-            mul*=-1;
-        }
+    else{
+        res=men(x);
     }
     printf("Y = %.2f",res);
 
