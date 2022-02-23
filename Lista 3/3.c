65aade62fb4a9d6884d362ad55d03ef23ec093ec
Caio <caioorebeo@gmail.com>
2022-02-23 16:42:27 -0300

    3 finalizada

--- Lista 3/3.c
+++ Lista 3/3.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
 
-int div(int n)
-{
+int div(int n){
+    
     int tot = 0;
+
     printf("Divisores de %d: ",n);
-    for (int c=1;c<=n;c++)
-    {
-        if (n%c==0)
-        {
+    for (int c=1;c<=n;c++){
+        if (n%c==0){
             printf(" %d ", c);
             tot++;
         }
     }
     return tot;
 }
+long int fat(int n){
+
+    int nfat;
+
+    if(n==0){
+        return 1;
+    }
+    else{
+        return n*fat(n-1);
+    }
+
+}
+int nms(int n){
+
+    int som=0;
+    for(int con=1;con<=n;con++){
+        som+=con;
+    }
+    return som;
+}
 
 int main(){
 
     int n;
 
-    printf("Número(negativo para): ");
-    scanf("%d",&n);
+    while(1){
 
-    if (n%2==0){
-        printf("= %d divisores\n",div(n));
+        printf("Se for par verifica divisores.\nSe impar e < que 10 calcula fatorial.\nSe impar e maior ou igual a 10\n");
+        printf("Número(negativo para):");
+        scanf("%d",&n);
+
+        if (n<0){
+            break;
+        }
+        if (n%2==0){
+            printf("= %d divisores\n",div(n));
+        }
+        else if(n%2!=0 && n<10){
+            printf("%d! = %ld\n",n,fat(n));
+        }
+        else if(n%2!=0 && n>=10){
+            printf("Soma dos inteiros de 1 até n = %d",nms(n));
+        }
     }
-
 }
