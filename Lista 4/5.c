0533e030bda8da540475326bbf3de4d60f73fedf
Caio <caioorebeo@gmail.com>
2022-02-28 16:27:54 -0300

    5

---
+++ Lista 4/5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+   int mat[4][5],con;
+
+   for(int c=0;c<4;c++){
+
+      for(int c=0;c<5;c++){
+         printf("numero: ");
+         scanf("%d",&mat[0][c]);}
+
+      for(int c=0;c<5;c++){
+         printf("numero: ");
+         scanf("%d",&mat[1][c]);}
+
+      for(int c=0;c<5;c++){
+         printf("numero: ");
+         scanf("%d",&mat[2][c]);}
+         
+      for(int c=0;c<5;c++){
+         printf("numero: ");
+         scanf("%d",&mat[3][c]);}
+      
+
+   }
+       
+   
+
+}
