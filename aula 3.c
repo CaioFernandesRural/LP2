36a16bd5287653346bbe6e88c7aa67407c124251
Caio <caioorebeo@gmail.com>
2022-03-31 10:59:38 -0300

    aula 3 completa

---
+++ aula 3.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/*#define diga printf
+#define oi "\nOlá, tudo bem?"
+
+void main(){
+
+    diga(oi);
+
+}*/
+//--------------------
+/*float media(float a, float b){ //tira médiaa
+
+    float med;
+
+    med=(a+b)/2;
+    
+    return (med);
+}
+void main(){
+
+    float a, b;
+
+    puts("Digite dois números: ");
+    scanf("%f %f",&a,&b);
+
+    printf("\nMedia: %.2f",media(a,b));
+
+}*/
+//--------------------
+/*float a,b,med; //teste variáveis globais
+
+void media(void){
+    med=(a+b)/2;
+}
+void main(){
+    puts("Digite dois números: ");
+    scanf("%f %f",&a,&b);
+
+    media();
+
+    printf("\nMedia: %.2f",med);
+}*/
+//--------------------
+/*int fat(int n){ //fat de n
+    
+    int valor;
+
+    if(n==0){
+        valor=1;
+    }
+    else{
+        valor=n*fat(n-1);
+    }
+    return(valor);
+}
+void main(){
+
+    int n;
+
+    printf("Digite: ");
+    scanf("%d",&n);
+
+    printf("\n%d",fat(n));
+}*/
+//--------------------
