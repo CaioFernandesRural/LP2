eab241b0cdd0e99b03ed32322735a11d1ec26d1f
Caio <caioorebeo@gmail.com>
2022-04-09 14:24:22 -0300

    aula 6 alterada

---
+++ Lista 6/2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void criar()
+{
+
+}
+void incluir()
+{
+
+}
+void excluir()
+{
+
+}
+void listar()
+{
+
+}
+void calcular()
+{
+
+}
+void imprimir()
+{
+
+}
+void fechar()
+{
+
+}
+
+void main()
+{
+    int escolha;
+
+    while(1>0)
+    {
+        printf("\nESCOLHA:");
+        printf("\n1-CRIAR DISCIPLINA");
+        printf("\n2-INCLUIR ALUNO");
+        printf("\n3-EXCLUIR ALUNO");
+        printf("\n4-LISTAR ALUNOS");
+        printf("\n5-CALCULAR CR MEDIO");
+        printf("\n6-IMPRIMIR BOLETIM");
+        printf("\n7-FECHAR DISCIPLINA");
+        printf("\n8-SAIR");
+        scanf("%d",&escolha);
+        
+        
+    }
+}
