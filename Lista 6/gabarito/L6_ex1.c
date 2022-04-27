c2b4ca6e626f8a4a0167643e243fdaa9b8d209cf
Caio <caioorebeo@gmail.com>
2022-04-27 20:58:03 -0300

    deus

--- Lista 6/gabarito/L6_ex1.c
+++ Lista 6/gabarito/L6_ex1.c
@@ -1,12 +1,12 @@
 //Escreva um programa que cria um tipo de registro (usando typedef) que define uma data, 
-//formada pelos campos DIA, MÊS e ANO. Escreva também as funções:
-//• INICIALIZA, que recebe valores de dia, mês e ano, além de um registro do 
-//tipo data, passado por referência e atribui esses valores aos campos do registro;
-//• VALIDA, que recebe o registro data, passado por valor, e verifica se os 
-//valores de dia, mês e ano do registro são válidos; e
-//• IMPRIME, que recebe o registro data, passado por valor, e imprime seus campos.
-//Dentro da função main, declare uma variável do tipo data e chame cada uma das
-// funções descritas acima.
+//formada pelos campos DIA, Mï¿½S e ANO. Escreva tambï¿½m as funï¿½ï¿½es:
+//ï¿½ INICIALIZA, que recebe valores de dia, mï¿½s e ano, alï¿½m de um registro do 
+//tipo data, passado por referï¿½ncia e atribui esses valores aos campos do registro;
+//ï¿½ VALIDA, que recebe o registro data, passado por valor, e verifica se os 
+//valores de dia, mï¿½s e ano do registro sï¿½o vï¿½lidos; e
+//ï¿½ IMPRIME, que recebe o registro data, passado por valor, e imprime seus campos.
+//Dentro da funï¿½ï¿½o main, declare uma variï¿½vel do tipo data e chame cada uma das
+// funï¿½ï¿½es descritas acima.
 
 #include <stdio.h>
 
@@ -18,36 +18,35 @@ void inicializa (tpData * dt, int dia, int mes, int ano) {
 	dt->dia = dia;
 	dt->mes = mes;
 	dt->ano = ano;
-	
 }
 
 int valida (tpData dt) {
 	if (dt.ano > 2022 || dt.ano<1900) {
-		printf("Ano invalido!");  // poderia ter colocado mensangens de erro específicas também
-		return 0;  //  0 = false vai indicar que a data é inválida
+		printf("Ano invalido!");  // poderia ter colocado mensangens de erro especï¿½ficas tambï¿½m
+		return 0;  //  0 = false vai indicar que a data ï¿½ invï¿½lida
 	}
 	
 	if (dt.mes > 12 || dt.mes < 1) {
 		printf("Mes invalido!");
-		return 0;  //  0 = false vai indicar que a data é inválida
+		return 0;  //  0 = false vai indicar que a data ï¿½ invï¿½lida
 	}
 	
 	if (dt.dia < 1 || dt.dia > 31) {
 		printf("Dia invalido!");
-		return 0;  //  0 = false vai indicar que a data é inválida	
+		return 0;  //  0 = false vai indicar que a data ï¿½ invï¿½lida	
 	}
 	
 	if ((dt.mes == 4 || dt.mes == 6 || dt.mes == 9 || dt.mes == 11 ) && dt.dia > 30) {
 		printf("Dia invalido!");
-		return 0;  //  0 = false vai indicar que a data é inválida	
+		return 0;  //  0 = false vai indicar que a data ï¿½ invï¿½lida	
 	}	
 
 	if (dt.mes == 2 && dt.dia > 28) {  // nao vou pensar em anos bissextos...
 		printf("Dia invalido!");
-		return 0;  //  0 = false vai indicar que a data é inválida	
+		return 0;  //  0 = false vai indicar que a data ï¿½ invï¿½lida	
 	}	
 	
-	return 1; //  1 = true vai indicar que a data é válida	
+	return 1; //  1 = true vai indicar que a data ï¿½ vï¿½lida	
 }
 
 void imprime (tpData dt) {
