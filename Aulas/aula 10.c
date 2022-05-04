77cf97b6df0744da9d69dd648e792bdc6e5894d1
Caio <caioorebeo@gmail.com>
2022-05-04 11:06:39 -0300

    opa

---
+++ Aulas/aula 10.c
@@ -0,0 +1,15 @@
+#include "aula10.h"
+
+void main()
+{
+    int a, b;
+
+    printf("\nNumero: ");
+    scanf("%d",&a);
+    printf("\nNumero: ");
+    scanf("%d",&b);
+
+    printf("\n%d | %f",multiplicacao(a, b), divisao(a, b));
+
+
+}
---
+++ Aulas/aula10.h
@@ -0,0 +1,16 @@
+#ifndef __AULA10__
+#define __AULA10__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiplicacao(int a, int b)
+{
+    return (a * b);
+}
+float divisao(int a, int b)
+{
+    return (a / b);
+}
+
+#endif
